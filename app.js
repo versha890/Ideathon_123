7b19e7331fc1f78437ce6864b9af55ff5d4150e6
Vani <vanipandey2502@gmail.com>
2025-01-15 00:47:47 +0530

    contact,home

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const fs = require("fs");
 const path = require("path");
 const ejsmate = require("ejs-mate");
 
@@ -7,6 +9,10 @@ app.set("view engine" , "ejs");
 app.set("views" , path.join(__dirname , "views"));
 app.use(express.static(path.join(__dirname , "public")));
 app.engine("ejs" , ejsmate);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 app.listen(8080 , ()=>{
     console.log("app listening to 8080");
@@ -47,4 +53,28 @@ app.get("/main/end" , (req,res)=>{
 
 app.get("/main/learnmore", (req , res)=>{
     res.render("mains/learnmore.ejs");
+});
+
+app.get("/main/contact" , (req, res)=>{
+    res.render("mains/contact.ejs")
+});
+
+app.post("/submit", (req, res) => {
+    const name = req.body.name; // Access the "name" field from the form
+    const email = req.body.email; // Access the "email" field
+    const message = req.body.message; // Access the "message" field
+
+    console.log("Form Submitted!");
+    console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+    const filePath = path.join(__dirname, "public/files/formData.txt");
+    content =`\n${name},${email},${message}`
+    fs.appendFile(filePath, content, (err) => {
+        if (err) {
+            console.error("Error writing to file:", err);
+        }
+
+        console.log("Data written to file successfully!");
+        return res.render("mains/submit.ejs");
+    });
+
 });
